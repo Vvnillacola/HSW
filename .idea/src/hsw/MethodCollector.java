cae7051c330e423ed2456a8d24ad52d5ad9a56a5
benlu <benlukasknirsch2004@gmail.com>
2025-11-06 12:49:38 +0100

    Initial commit

---
+++ .idea/src/hsw/MethodCollector.java
@@ -0,0 +1,23 @@
+public class MethodCollector {
+
+    /**
+     * Die main-Methode ruft alle statischen
+     * Methoden nacheinander auf.
+     * @param args
+     */
+    public static void main(String[] args) {
+        int methodCounter = 1;
+        // Erste Methode
+        System.out.println("Method " + methodCounter + ":" );
+        aFirstMethod();
+        methodCounter++;
+// hier kann die nächste Methode folgen.
+    }
+
+
+    private static void aFirstMethod() {
+        System.out.println("MethodCollector.aFirstMethod: This method has been created by ");
+    }
+
+
+}
